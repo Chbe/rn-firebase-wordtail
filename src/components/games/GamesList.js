b4a311eef87c762b0b5d9d1095138b6e6e8d1c30
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2019-09-04 19:57:54 +0200

    firestore

--- src/components/games/GamesList.js
+++ src/components/games/GamesList.js
@@ -1,13 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { FlatList, Text } from 'react-native';
-import { Games } from '../../test/ExampleGames';
 import ActiveGameCard from './game-cards/ActiveGameCard';
+import firebase from 'react-native-firebase';
+
+
 
 
 const renderGameCard = ({ item }) => {
     return <ActiveGameCard game={item} />
 }
 const GamesList = ({ uid }) => {
+    const [games, setGames] = useState([]);
+
+    const onCollectionUpdate = (querySnapshot) => {
+        const todos = [];
+        querySnapshot.forEach((doc) => {
+            const game = doc.data();
+
+            todos.push({
+                key: doc.id,
+                ...game
+            });
+        });
+        setGames(todos);
+    }
+    useEffect(() => {
+        const ref = firebase.firestore().collection('games');
+
+        const sub = ref.onSnapshot(onCollectionUpdate)
+        return () => {
+            sub();
+        };
+    }, [])
 
 
     // Games.filter(game => game.status !== 'completed'
@@ -15,7 +39,7 @@ const GamesList = ({ uid }) => {
     //                 p.uid === userUid && p.accepted === true))
     return (
         <FlatList
-            data={Games}
+            data={games}
             renderItem={renderGameCard}
         />
     )
