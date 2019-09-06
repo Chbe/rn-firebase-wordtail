200859088fee5fb9e4220f1373be6f9a13a52638
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2019-09-06 16:56:45 +0200

    nativebase install.

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView, View } from 'react-native'
-import { Text, Card, Avatar, Image, Input } from 'react-native-elements'
+// import { Text, Card, Avatar, Image, Input } from 'react-native-elements'
 import Logout from '../components/auth/logout/Logout'
 import { CenterView } from '../components/UI/Containers/Containers'
 import firebase, { RNFirebase } from 'react-native-firebase'
+import { Content, Form, Item, Input, Label, Container } from 'native-base'
+import { Avatar } from 'react-native-elements'
 
 const ProfilePage = ({ navigation }) => {
     const [user, setUser] = useState({});
@@ -19,10 +21,10 @@ const ProfilePage = ({ navigation }) => {
         };
     }, [])
     return (
-        <SafeAreaView>
-            <Logout navigation={navigation} />
-            <View>
-                <Card title="title">
+        <SafeAreaView style={{ flex: 1 }}>
+            <Container>
+                <Logout navigation={navigation} />
+                <Content>
                     <View>
                         <Avatar
                             size='large'
@@ -33,11 +35,19 @@ const ProfilePage = ({ navigation }) => {
                                 : null}
                             showEditButton
                         />
-                        <Input placeholder={displayName} />
-                        <Input placeholder={email} />
                     </View>
-                </Card>
-            </View>
+                    <Form>
+                        <Item floatingLabel>
+                            <Label>{displayName}</Label>
+                            <Input />
+                        </Item>
+                        <Item floatingLabel last>
+                            <Label>{email}</Label>
+                            <Input />
+                        </Item>
+                    </Form>
+                </Content>
+            </Container>
         </SafeAreaView>
     )
 }
