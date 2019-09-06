6c49c3917e4989fa23ef5117939ca48af2c8044f
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2019-09-06 10:09:57 +0200

    createAnimatedSwitchNavigator

--- src/App.js
+++ src/App.js
@@ -1,67 +1,16 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow
-//  */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import React from 'react'
 
-// import React, { Fragment, useEffect, useState } from 'react';
-// import {
-//   Text,
-//   View
-// } from 'react-native';
-
-// import firebase from 'react-native-firebase';
-// import { SafeWrapper, CenterView } from './components/UI/Containers/Containers';
-// import LoginPage from './pages/LoginPage';
-// import Logout from './components/auth/logout/Logout'
-// import HomePage from './pages/HomePage';
-// import { ThemeProvider, colors } from 'react-native-elements';
-// import { Platform } from 'react-native';
-// const theme = {
-//   colors: {
-//     ...Platform.select({
-//       default: colors.platform.android,
-//       ios: colors.platform.ios,
-//     }),
-//   },
-// };
-// const App = () => {
-//   const [user, setUser] = useState(null);
-//   const [isLoading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const unsubscriber = firebase.auth().onAuthStateChanged((user) => {
-//       setUser(user);
-//       setLoading(false);
-//     });
-//     return () => {
-//       if (unsubscriber)
-//         unsubscriber();
-//     };
-//   }, [])
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <SafeWrapper>
-//         <View>
-//           {!isLoading
-//             ? !user
-//               ? <LoginPage />
-//               : <HomePage user={user} />
-//             : <CenterView><Text>Loading...</Text></CenterView>}
-//         </View>
-//       </SafeWrapper>
-//     </ThemeProvider>
-//   );
-// };
-
-// export default App;
-
-import React from 'react';
-import { View, Text } from 'react-native';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
+import { Transition } from 'react-native-reanimated';
 import AuthLoadingPage from './pages/AuthLoadingPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -69,7 +18,7 @@ import LoginPage from './pages/LoginPage';
 const AppStack = createStackNavigator({ Home: HomePage });
 const AuthStack = createStackNavigator({ Login: LoginPage });
 
-const AppNavigator = createSwitchNavigator(
+const AppNavigator = createAnimatedSwitchNavigator(
   {
     AuthLoading: AuthLoadingPage,
     App: AppStack,
@@ -77,6 +26,19 @@ const AppNavigator = createSwitchNavigator(
   },
   {
     initialRouteName: 'AuthLoading',
+  },
+  {
+    // The previous screen will slide to the bottom while the next screen will fade in
+    transition: (
+      <Transition.Together>
+        <Transition.Out
+          type="slide-bottom"
+          durationMs={100}
+          interpolation="easeIn"
+        />
+        <Transition.In type="fade" durationMs={200} />
+      </Transition.Together>
+    ),
   }
 )
 const App = createAppContainer(AppNavigator);
