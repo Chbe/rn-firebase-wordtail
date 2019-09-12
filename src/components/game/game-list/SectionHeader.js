3ab53fb82760aca89bdaa07069b65f638053bb90
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2019-09-12 23:08:23 +0200

    game list css, theme colors updates

--- src/components/game/game-list/SectionHeader.js
+++ src/components/game/game-list/SectionHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native-elements';
+import { Text, withTheme } from 'react-native-elements';
 import { PaddingView } from '../../UI/Containers/Containers';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import styled from 'styled-components';
@@ -7,36 +7,42 @@ import styled from 'styled-components';
 const SectionView = styled(PaddingView)`
     flex-direction: row;
     align-items: center;
-    background-color: #e6e6e6;
+    background-color: ${props => props.theme.colors.info};
 `;
 
 const SectionIcon = styled(Icon)`
     margin-right: 10px;
 `;
 
-const SectionHeader = ({ section: { type, data } }) => {
+const ColoredText = styled(Text)`
+    color: ${props => props.textColor};
+    font-weight: bold
+`;
+
+const SectionHeader = ({ section: { type, data }, theme }) => {
+    const textColor = theme.barStyle === 'light-content' ? '#fff' : '#000';
     const length = data.length;
     if (type === 'invite')
         return (
-            <SectionView>
-                <SectionIcon size={24} name={'envelope'} regular />
-                <Text>{length === 1 ? 'New invite' : 'New invites'}</Text>
+            <SectionView theme={theme}>
+                <SectionIcon color={textColor} size={24} name={'envelope'} regular />
+                <ColoredText textColor={textColor}>{length === 1 ? 'New invite' : 'New invites'}</ColoredText>
             </SectionView>
         )
     else if (type === 'active')
         return (
-            <SectionView>
-                <SectionIcon size={24} name={'gamepad'} regular />
-                <Text>Active or pending</Text>
+            <SectionView theme={theme}>
+                <SectionIcon color={textColor} size={24} name={'gamepad'} regular />
+                <ColoredText textColor={textColor}>Active or pending</ColoredText>
             </SectionView>
         )
     else if (type === 'finished')
         return (
-            <SectionView>
-                <SectionIcon size={24} name={'flag-checkered'} regular />
-                <Text>Finished</Text>
+            <SectionView theme={theme}>
+                <SectionIcon color={textColor} size={24} name={'flag-checkered'} regular />
+                <ColoredText textColor={textColor}>Finished</ColoredText>
             </SectionView>
         )
 }
 
-export default SectionHeader
+export default withTheme(SectionHeader)
