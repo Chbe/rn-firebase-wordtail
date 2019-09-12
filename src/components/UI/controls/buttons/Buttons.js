645234f10d55cba6b927bc0f8ba6648f9694a142
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2019-09-12 10:31:22 +0200

    custom components tempalte test

--- src/components/UI/controls/buttons/Buttons.js
+++ src/components/UI/controls/buttons/Buttons.js
@@ -1,6 +1,46 @@
-import styled from 'styled-components/native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import React from 'react';
+import styled from 'styled-components';
+import { View } from 'react-native';
 
-export const LoginButton = styled(Icon.Button)`
-    padding: 10px;
+const CustomButton = ({ title, color, onPress, textColor, disabled, icon }) => {
+    return (
+        <ButtonContainer
+            color={color}
+            disabled={disabled}
+            activeOpacity={0.85}
+            disabledColor={`${color}-disabled`}
+            onPress={() => onPress ? onPress() : null}
+        >
+            <View style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'row',
+                width: '100%'
+            }}>
+                {/* {icon && <View style={{ marginRight: 10 }}>
+                    <FontAwesomeIcon icon={icon} color={textColor} size={18} />
+                </View>} */}
+
+                <ButtonText textColor={textColor}>{title}</ButtonText>
+            </View>
+        </ButtonContainer>
+    );
+}
+
+export default CustomButton;
+
+const ButtonContainer = styled.TouchableOpacity`
+    justify-content: center;
+    flex-direction: row;
+	padding: ${props => props.theme.padding};
+	border-radius: ${props => props.theme.borderRadius};	
+    background-color: ${props => !props.disabled
+        ? props.theme.colors[props.color]
+        : props.theme.colors[props.color + '-disabled']};
+`;
+
+const ButtonText = styled.Text`
+	font-size: ${props => props.theme.fontSize};
+	color: ${props => props.textColor};
+	text-align: center;
 `;
