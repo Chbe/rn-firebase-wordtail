632f0d0f6196345a04824b9fb073b4fd551e24fb
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2019-09-13 21:57:24 +0200

    Template Pages

---
+++ src/pages/ChatPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SafeWrapper, PaddingView } from '../components/UI/Containers/Containers'
+import { Text } from 'react-native'
+
+const ChatPage = () => {
+    return (
+        <SafeWrapper>
+            <PaddingView>
+                <Text>TODO Chat Page</Text>
+            </PaddingView>
+        </SafeWrapper>
+    )
+}
+
+export default ChatPage
---
+++ src/pages/ScorePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SafeWrapper, PaddingView } from '../components/UI/Containers/Containers'
+import { Text } from 'react-native'
+
+const ScorePage = () => {
+    return (
+        <SafeWrapper>
+            <PaddingView>
+                <Text>TODO Score Page</Text>
+            </PaddingView>
+        </SafeWrapper>
+    )
+}
+
+export default ScorePage
